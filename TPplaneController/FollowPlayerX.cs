e74c5638cb8caff8fd7153e5d69efbd04dd6150f
kaan-erk <kaanerktoraman@gmail.com>
2024-08-17 16:52:10 +0300

    Third Person Plane Controller

---
+++ TPplaneController/FollowPlayerX.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Net.Mail;
+using UnityEngine;
+
+public class KeyPressCooldown : MonoBehaviour
+{
+    public KeyCode toggleKey = KeyCode.Space;
+    public Transform newCameraPosition;
+    public float cooldownTime = 1f;
+
+    private Vector3 defaultPosition;
+    private Quaternion defaultRotation;
+    private bool isPositionChanged = false;
+    private bool isOnCooldown = false;
+    private float cooldownEndTime;
+
+    private void Start()
+    {
+        defaultPosition = Camera.main.transform.position;
+        defaultRotation = Camera.main.transform.rotation;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(toggleKey) && !isOnCooldown)
+        {
+            ToggleCameraPosition();
+            StartCooldown();
+        }
+
+        UpdateCooldown();
+    }
+
+    private void ToggleCameraPosition()
+    {
+        if (isPositionChanged)
+        {
+            Camera.main.transform.position = defaultPosition;
+            Camera.main.transform.rotation = defaultRotation;
+        }
+        else
+        {
+            if (newCameraPosition != null)
+            {
+                Camera.main.transform.position = newCameraPosition.position;
+                Camera.main.transform.rotation = newCameraPosition.rotation;
+            }
+        }
+
+        isPositionChanged = !isPositionChanged;
+    }
+
+    private void StartCooldown()
+    {
+        isOnCooldown = true;
+        cooldownEndTime = Time.time + cooldownTime;
+    }
+
+    private void UpdateCooldown()
+    {
+        if (isOnCooldown && Time.time >= cooldownEndTime)
+        {
+            isOnCooldown = false;
+        }
+    }
+}
---
+++ TPplaneController/PlayerControllerX.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 20;
+    public float rotateSpeed = 20;
+    public float verticalInput;
+    public float horizontalInput;
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Rotate(Vector3.left, Time.deltaTime * rotateSpeed * verticalInput);
+        transform.Rotate(Vector3.back, Time.deltaTime * rotateSpeed * horizontalInput);
+        //transform.Rotate(Vector3.left, Time.deltaTime * -5); 
+    }
+}
